45baabfd23fe7f96d2d58665dfe2e8dd58a25f92
Freebet98 <108910779+Freebet98@users.noreply.github.com>
2023-10-31 08:33:54 -0500

    Update Language.py

--- Language.py
+++ Language.py
@@ -1,2 +1,11 @@
-language_dict = {}
-language_ans_dict = {}
+language_dict = {'How many writing systems does the Japanese Language Use?': 'Three',
+				  'What does the Latin Phrase “quid pro quo” mean?': 'Something for something',
+                'What are the two most common languages spoken in South America?': 'Spanish and Portuguese',
+                'What area of study, signifies the “art of technique of arranging type to make written language legible, readable, and appealing?': 'Typography'
+                	}
+language_ans_dict = {'Three': ['Three', 'Five', 'One', 'Four'],
+'Something for something': ['Something for something', 'It is what it is', 'An arm for a leg', 'An eye for an eye'],
+'Spanish and Portuguese' : ['Spanish and Portuguese', 'Spanish and English', 'English and Portuguese', 'Spanish and French'],
+'Typography': ['Typography'],
+
+}
